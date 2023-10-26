bc9b6eba30c67a3a1ee15320974f40ccc5597aa3
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-10-26 19:16:48 -0300

    Trabalhando com for e list

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -3,16 +3,27 @@
 using System.Globalization;
 
 namespace Course {
+
+    class Aluno {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+
     class Program {
         static void Main(string[] args) {
-            double a;
-            float b;
 
-            a = 5.1;
-            b = ((float)a);
+            List<Aluno> alunos = new List<Aluno> {
+                { new Aluno { Name = "João", Grade = 8 } },
+                { new Aluno { Name = "Maria", Grade = 3 } },
+                { new Aluno { Name = "Pedro", Grade = 9 } },
+                { new Aluno { Name = "Ana", Grade = 5 } },
+                { new Aluno { Name = "Carlos", Grade = 10 } },
+            };
 
-            Console.WriteLine(b);
-
+            foreach (Aluno aluno in alunos) {
+                string gradeSituation = aluno.Grade >= 6 ? "APROVADO!" : "REPROVADO!";
+                Console.WriteLine($"O aluno(a) {aluno.Name} está {gradeSituation}");
+            }
 
 
         }

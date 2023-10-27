b45e154a48c3a567dc2485d6f18b10c53efd060e
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-10-27 17:33:44 -0300

    Utilizando classe para representar um produto

---
+++ Course/Course/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course {
+    class Product {
+        public string Name;
+        public double Price;
+        public int Unity;
+
+        public double TotalPriceInStock() {
+            return Price * Unity;
+        }
+    }
+}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,39 +7,33 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Triangle x, y;
-
-            x = new Triangle();
-            y = new Triangle();
+            Product p = new Product();
             
-            Console.WriteLine("Informe a altura do triângulo X:");
-            x.Height = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Informe a base do triângulo X:");
-            x.Base = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Informe a altura do triângulo Y:");
-            y.Height = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Informe a base do triângulo Y:");
-            y.Base = int.Parse(Console.ReadLine());
-
-
-            double xArea = x.calculateArea();
-
-            double yArea = y.calculateArea();
-
+            Console.WriteLine("Digite os dados do produto: ");
             
+            Console.WriteLine("Nome: ");
+            p.Name = Console.ReadLine();
+            
+            Console.WriteLine("Digite o preço: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            Console.WriteLine("Digite as unidades em estoque: ");
+            p.Unity = int.Parse(Console.ReadLine());
 
-            string biggerTriangle = xArea > yArea ? "X" : "Y";
+            Console.WriteLine($"Dados do produto: \nNome: {p.Name}\nPreço: R${p.Price}\n" +
+                $"Unidades: {p.Unity}\nTotal: R${p.TotalPriceInStock()}\n\n");
 
-            Console.WriteLine($"O triângulo {biggerTriangle} é o maior!");
-            Console.WriteLine($"Área X: {xArea:F2}");
-            Console.WriteLine($"Área Y: {yArea:F2}");
+            Console.WriteLine("Digite as unidades a serem adicionadas em estoque: ");
+            p.Unity += int.Parse(Console.ReadLine());
 
+            Console.WriteLine($"Dados atualizados do produto: \nNome: {p.Name}\nPreço: R${p.Price}\n" +
+                $"Unidades: {p.Unity}\nTotal: R${p.TotalPriceInStock()}\n\n");
 
+            Console.WriteLine("Digite as unidades a serem removidas do estoque: ");
+            p.Unity -= int.Parse(Console.ReadLine());
 
+            Console.WriteLine($"Dados atualizados do produto: \nNome: {p.Name}\nPreço: R${p.Price}\n" +
+                $"Unidades: {p.Unity}\nTotal: R${p.TotalPriceInStock()}\n\n");
         }
     }
 }

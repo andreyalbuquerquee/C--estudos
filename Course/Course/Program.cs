62f84e656a765d67f4473b2ef90827a68e9e6196
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-10-31 22:41:08 -0300

    Relacionando classes

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -9,21 +9,52 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Order order = new Order {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-            };
+            Console.WriteLine("Enter deparment's name");
+            string departmentName = Console.ReadLine();
 
-            Console.WriteLine(order);
+            Console.WriteLine("Enter worker data: ");
+            Console.WriteLine("Name: ");
+            string workerName = Console.ReadLine();
+            
+            Console.WriteLine("Level: (Junior, MidLevel, Senior)");
+            WorkerLevel workerLevel = Enum.Parse<WorkerLevel>(Console.ReadLine());
 
-            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine(txt);
+            Department department = new Department(departmentName);
+            Worker worker = new Worker(workerName, workerLevel, baseSalary, department);
 
-            OrderStatus orderStatus = Enum.Parse<OrderStatus>("Shipped");
+            Console.WriteLine("How many contracts for this worker?");
+            int numberOfContracts = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(orderStatus);
+            for(int i = 1; i <= numberOfContracts; i++) {
+                Console.WriteLine($"Enter the #{i} contract: ");
+                Console.WriteLine("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract workerHourContract = new HourContract(date, valuePerHour, hours);
+
+                worker.AddContract(workerHourContract);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter month and year to calculate income (MM/YYYY): ");
+            string monthYearInput = Console.ReadLine();
+            string[] monthAndYear = monthYearInput.Split("/");
+            int month = int.Parse(monthAndYear[0]);
+            int year = int.Parse(monthAndYear[1]);
+
+            Console.WriteLine($"Name: {worker.Name}");
+            Console.WriteLine($"Department: {worker.Department.Name}");
+            Console.WriteLine($"Income for {monthYearInput}: {worker.Income(year, month)}");
+
 
 
         }

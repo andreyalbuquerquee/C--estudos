4f06d84dd4c37863d730d109f2ff6918c1b09085
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-01 02:54:39 -0300

    Mais um exercício relacionando classes e utilizando um cara chamado StringBuilder

--- Course/Course/Entities/Enums/WorkerLevel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Course.Entities.Enums {
-    public enum WorkerLevel: int {
-        Junior = 0,
-        MidLevel = 1,
-        Senior = 2,
-    }
-}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,62 +1,24 @@
-﻿
-
-using Course.Entities;
-using Course.Entities.Enums;
-using System.Globalization;
+﻿using Course.Entities;
 
 namespace Course {
 
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Enter deparment's name");
-            string departmentName = Console.ReadLine();
+            Comment c1 = new Comment("Have a nice trip!");
 
-            Console.WriteLine("Enter worker data: ");
-            Console.WriteLine("Name: ");
-            string workerName = Console.ReadLine();
-            
-            Console.WriteLine("Level: (Junior, MidLevel, Senior)");
-            WorkerLevel workerLevel = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Comment c2 = new Comment("Wow that's awesome!");
 
-            Console.WriteLine("Base Salary: ");
-            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"), 
+                "Traveling to New Zeland", 
+                "I'm going to visit this",
+                12);
 
-            Department department = new Department(departmentName);
-            Worker worker = new Worker(workerName, workerLevel, baseSalary, department);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
 
-            Console.WriteLine("How many contracts for this worker?");
-            int numberOfContracts = int.Parse(Console.ReadLine());
-
-            for(int i = 1; i <= numberOfContracts; i++) {
-                Console.WriteLine($"Enter the #{i} contract: ");
-                Console.WriteLine("Date (DD/MM/YYYY): ");
-                DateTime date = DateTime.Parse(Console.ReadLine());
-
-                Console.WriteLine("Value per hour: ");
-                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Console.WriteLine("Duration (hours): ");
-                int hours = int.Parse(Console.ReadLine());
-
-                HourContract workerHourContract = new HourContract(date, valuePerHour, hours);
-
-                worker.AddContract(workerHourContract);
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Enter month and year to calculate income (MM/YYYY): ");
-            string monthYearInput = Console.ReadLine();
-            string[] monthAndYear = monthYearInput.Split("/");
-            int month = int.Parse(monthAndYear[0]);
-            int year = int.Parse(monthAndYear[1]);
-
-            Console.WriteLine($"Name: {worker.Name}");
-            Console.WriteLine($"Department: {worker.Department.Name}");
-            Console.WriteLine($"Income for {monthYearInput}: {worker.Income(year, month)}");
-
-
-
+            Console.WriteLine(p1);
         }
     }
 }

4e07e7c355ef2d9e0794c888f73c6bcdf5645b8f
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-03 13:19:32 -0300

    override e virtual

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,24 +5,14 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Account acc = new Account(1001, "Andrey", 0.0);
+            Account acc1 = new Account(1001, "Andrey", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Ana", 500.0, 0.01);
 
-            BusinessAccount bacc = new BusinessAccount(1002, "Brian", 0.0, 500.0);
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
 
-            // UPCASTING
-            Account acc1 = bacc;
-            Account acc2 = new BusinessAccount(1003, "João", 0.0, 200.0);
-            Account acc3 = new SavingsAccount(1004, "Caju", 0.0, 0.01);
-
-            // DOWNCASTING
-            BusinessAccount acc4 = (BusinessAccount)acc2;
-
-            // BusinessAccount acc5 = (BusinessAccount)acc3; Da erro pq BusinessAccount n é compatível com SavingsAccount
-            if (acc3 is BusinessAccount) // Vai dar false
-            {
-                BusinessAccount acc5 = (BusinessAccount)acc3;
-                BusinessAccount acc6 = acc3 as BusinessAccount; // da pra fazer assim tb o casting
-            }
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
         }
     }
 }

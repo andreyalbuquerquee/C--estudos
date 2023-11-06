521e42d06d1730a9e9c5bede8fd89024b710f19d
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-06 20:30:03 -0300

    lançando erros de uma forma ruim

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,27 +1,43 @@
-﻿namespace Course
+﻿using Course.Entities;
+
+namespace Course
 {
 
     class Program
     {
         static void Main(string[] args)
         {
-            try
+            Console.WriteLine("Room number");
+            int roomNumber = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Check-in date (dd/MM/yyyy):");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Check-out date (dd/MM/yyyy):");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
+            Console.WriteLine(reservation);
+
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.WriteLine("Check-in date (dd/MM/yyyy):");
+            checkIn = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Check-out date (dd/MM/yyyy):");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            string error = reservation.UpdateDates(checkIn, checkOut);
+
+            if (error != null) 
             {
-                int n1 = int.Parse(Console.ReadLine());
-                int n2 = int.Parse(Console.ReadLine());
-                
-                int result = n1 / n2;
+                Console.WriteLine(error);
             }
-            catch (DivideByZeroException e)
+            else
             {
-                Console.WriteLine(e.Message);
-            }
-            catch (FormatException e) 
-            {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(reservation);
             }
 
-            
         }
     }
 }

26bbc51722dabb33b03444ef6bdfe634ddef152a
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-06 22:07:54 -0300

    Exceptions do jeito certo

--- Course/Course/Entities/Reservation.cs
+++ Course/Course/Entities/Reservation.cs
@@ -12,6 +12,11 @@
 
         public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
         {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date!");
+            }
+
             RoomNumber = roomNumber;
             CheckIn = checkIn;
             CheckOut = checkOut;
@@ -24,23 +29,21 @@
             return (int)duration.TotalDays;
         }
 
-        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
         {
             DateTime now = DateTime.Now;
 
             if (checkIn < now) 
             {
-                return "Reservation dates for update must be future dates!"; 
+                throw new DomainException("Reservation dates for update must be future dates!"); 
             }
             if (checkOut <= checkIn) 
             {
-                return "Check-out date must be after check-in date!";
+                throw new DomainException("Check-out date must be after check-in date!");
             }
             
             CheckIn = checkIn;
             CheckOut = checkOut;
-
-            return null;
         }
         
         public override string ToString()
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,35 +7,40 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Room number");
-            int roomNumber = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Check-in date (dd/MM/yyyy):");
-            DateTime checkIn = DateTime.Parse(Console.ReadLine());
-
-            Console.WriteLine("Check-out date (dd/MM/yyyy):");
-            DateTime checkOut = DateTime.Parse(Console.ReadLine());
-
-            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
-
-            Console.WriteLine(reservation);
-
-            Console.WriteLine("Enter data to update the reservation: ");
-            Console.WriteLine("Check-in date (dd/MM/yyyy):");
-            checkIn = DateTime.Parse(Console.ReadLine());
-
-            Console.WriteLine("Check-out date (dd/MM/yyyy):");
-            checkOut = DateTime.Parse(Console.ReadLine());
-
-            string error = reservation.UpdateDates(checkIn, checkOut);
-
-            if (error != null) 
+            try
             {
-                Console.WriteLine(error);
-            }
-            else
-            {
+                Console.WriteLine("Room number");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Check-in date (dd/MM/yyyy):");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Check-out date (dd/MM/yyyy):");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
                 Console.WriteLine(reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.WriteLine("Check-in date (dd/MM/yyyy):");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Check-out date (dd/MM/yyyy):");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+
+                Console.WriteLine(reservation);
+            }
+            catch (DomainException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
             }
 
         }

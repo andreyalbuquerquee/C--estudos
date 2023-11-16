9789d4e517cde6b0f65a67b8dd1469bc2e196b4e
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-16 19:00:03 -0300

    Trabalhando com arquivos

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,48 +1,27 @@
-﻿using Course.Entities;
+﻿using System.IO;
 
 namespace Course
 {
-
     class Program
     {
         static void Main(string[] args)
         {
+            string sourcePath = @"C:\Users\andre\OneDrive\Documents\C# estudos\file1.txt";
+            string targetPath = @"C:\Users\andre\OneDrive\Documents\C# estudos\file2.txt";
+
             try
             {
-                Console.WriteLine("Room number");
-                int roomNumber = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("Check-in date (dd/MM/yyyy):");
-                DateTime checkIn = DateTime.Parse(Console.ReadLine());
-
-                Console.WriteLine("Check-out date (dd/MM/yyyy):");
-                DateTime checkOut = DateTime.Parse(Console.ReadLine());
-
-                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
-
-                Console.WriteLine(reservation);
-
-                Console.WriteLine();
-                Console.WriteLine("Enter data to update the reservation: ");
-                Console.WriteLine("Check-in date (dd/MM/yyyy):");
-                checkIn = DateTime.Parse(Console.ReadLine());
-
-                Console.WriteLine("Check-out date (dd/MM/yyyy):");
-                checkOut = DateTime.Parse(Console.ReadLine());
-
-                reservation.UpdateDates(checkIn, checkOut);
-
-                Console.WriteLine(reservation);
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] fileLines = File.ReadAllLines(sourcePath);
+                Console.WriteLine($"Primeira linha: {fileLines[0]}");
+                Console.WriteLine($"Segunda linha: {fileLines[1]}");
             }
-            catch (DomainException ex)
+            catch (IOException ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            catch (FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
+        
         }
     }
 }

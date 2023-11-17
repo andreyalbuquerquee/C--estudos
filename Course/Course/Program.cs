bdd4adfd7f0ecbbce8bbcbb6b6a4cdb28a8049b4
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-16 21:59:59 -0300

    Trabalhando com FileStream e StreamReader

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,21 +7,26 @@ namespace Course
         static void Main(string[] args)
         {
             string sourcePath = @"C:\Users\andre\OneDrive\Documents\C# estudos\file1.txt";
-            string targetPath = @"C:\Users\andre\OneDrive\Documents\C# estudos\file2.txt";
+            StreamReader sr = null;
 
             try
             {
-                FileInfo fileInfo = new FileInfo(sourcePath);
-                fileInfo.CopyTo(targetPath);
-                string[] fileLines = File.ReadAllLines(sourcePath);
-                Console.WriteLine($"Primeira linha: {fileLines[0]}");
-                Console.WriteLine($"Segunda linha: {fileLines[1]}");
+                sr = File.OpenText(sourcePath);
+                
+                while(!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }  
             }
-            catch (IOException ex)
+            catch(IOException ex)
             {
                 Console.WriteLine(ex.Message);
             }
-        
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
         }
     }
 }

b8680f9fbe675700c611b8583d5f52f818a1c18c
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-18 22:50:38 -0300

    Exercício sem interface

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,27 +1,37 @@
 ﻿
 
+using Course.Entities;
+using Course.Services;
+using System.Globalization;
+
 namespace Course
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\andre\OneDrive\Documents\C# estudos";
+            Console.WriteLine("Enter rental data: ");
+            Console.WriteLine("Car Moldel: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
-            try
-            {
-                IEnumerable<string> folders = Directory.EnumerateFileSystemEntries(path, "*.cs", SearchOption.AllDirectories);
-                foreach (string folder in folders) 
-                {
-                    Console.WriteLine(folder);
-                }
-                Directory.CreateDirectory(path + @"\teste");
+            Console.WriteLine("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            }
-            catch (IOException ex) 
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Console.WriteLine("Enter price per day: ");
+            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(pricePerHour, pricePerDay);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine(carRental.Invoice);
+
         }
     }
 }
---
+++ Course/Course/Services/BrazilTaxService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Course.Services
+{
+    class BrazilTaxService
+    {
+        public double Tax(double amount)
+        {
+            if (amount > 100)
+            {
+                return amount * 0.15;
+            }
+            return amount * 0.2;
+        }
+    }
+}

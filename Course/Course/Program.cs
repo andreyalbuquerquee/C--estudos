995260595386c848109f78dc20956a17ff2f1397
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-22 10:00:52 -0300

    Utilizando HashSet

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,18 +1,16 @@
-﻿using Course.Entities;
-
-namespace Course
+﻿namespace Course
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Client a = new Client { Name = "Maria", Email = "maria@gmail.com"};
-            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+            HashSet<string> set = new HashSet<string>();
 
-            Console.WriteLine(a.Equals(b));
-            Console.WriteLine(a == b); // Aqui da diferente por causa da referência de memória
-            Console.WriteLine(a.GetHashCode());
-            Console.WriteLine(b.GetHashCode());
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("TV"));
         }
     }
 }

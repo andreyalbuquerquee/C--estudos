b52cdd7d0a9e854cab3d52ac5426dbf945f34c70
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-23 19:33:23 -0300

    Extension methods

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,23 +5,8 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> cookies = new Dictionary<string, string>();
-
-            cookies["user"] = "Maria";
-            cookies["email"] = "maria@gmail.com";
-            cookies["phone"] = "99775522";
-            cookies["phone"] = "76545451";
-
-            Console.WriteLine(cookies["phone"]);
-
-            cookies.Remove("email");
-
-            Console.WriteLine(cookies.ContainsKey("email"));
-
-            foreach (KeyValuePair<string, string> item in cookies) 
-            {
-                Console.WriteLine(item.Key + " : " + item.Value);
-            }
+            DateTime dt = new DateTime(2023, 11, 22, 20, 0, 0);
+            Console.WriteLine(dt.ElapsedTime());
         }
     }
 }

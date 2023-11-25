2e1f862c288a82139fc52a926d70733f72657538
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-11-25 14:15:17 -0300

    Comparasion do método sort, e expressão lambda

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,12 +1,27 @@
 ﻿
+using Course.Entities;
+
 namespace Course
 {
     class Program
     {
         static void Main(string[] args)
         {
-            DateTime dt = new DateTime(2023, 11, 22, 20, 0, 0);
-            Console.WriteLine(dt.ElapsedTime());
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Notebook", 1200.00));
+            products.Add(new Product("Tablet", 450.00));
+
+            
+            products.Sort((p1, p2) => p1.Price.CompareTo(p2.Price));
+
+            foreach (Product product in products) 
+            {
+                Console.WriteLine(product);
+            }
         }
+
+
     }
 }
